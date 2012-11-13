b8af3508f1e46c215167631c57a771615734fb1f
James Parham <jamey@theparhams.net>
2012-11-12 22:32:55 -0600

    Added methods for directory selection and changed update method to initialization method

--- src/ui/contactform/RotcContactsController.java
+++ src/ui/contactform/RotcContactsController.java
@@ -1,6 +1,11 @@
 
 package ui.contactform;
 
+import configuration.RotcPreferences;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 /**
  *
  * @author javarobots
@@ -18,8 +23,39 @@ public class RotcContactsController {
      * default location. Then update the model with appropriate file
      * path information.
      */
-    public void checkConfiguration() {
-
+    public void performInitialization() {
+        RotcPreferences prefs = RotcPreferences.getRotcPreferences();
+        model.setWorkingDirectoryDefined(prefs.getWorkDirSet());
+        model.setSynchronizationDirectoryDefined(prefs.getSyncDirSet());
+        model.notifyObservers();
+    }
+    
+    public void selectWorkingDirectory(JFrame frame) {
+        RotcPreferences prefs = RotcPreferences.getRotcPreferences();
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setSelectedFile(new File(prefs.getWorkDir()));
+        int success = chooser.showDialog(frame, "Select");
+        if (0 == success){
+            prefs.setWorkDir(chooser.getSelectedFile().getAbsolutePath());
+            prefs.setWorkDirSet(true);
+            model.setWorkingDirectoryDefined(true);
+            model.notifyObservers();
+        }
+    }
+    
+    public void selectSyncDirectory(JFrame frame) {
+        RotcPreferences prefs = RotcPreferences.getRotcPreferences();
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setSelectedFile(new File(prefs.getSyncDir()));
+        int success = chooser.showDialog(frame, "Select");
+        if (0 == success){
+            prefs.setSyncDir(chooser.getSelectedFile().getAbsolutePath());
+            prefs.setSyncDirSet(true);
+            model.setSynchronizationDirectoryDefined(true);
+            model.notifyObservers();
+        }
     }
 
 }
