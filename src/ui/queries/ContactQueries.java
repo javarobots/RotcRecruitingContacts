1dd2ad7974a1646282c6743bb31e0703597c569d
James Parham <jamey@theparhams.net>
2012-11-19 21:40:27 -0600

    Added queries for academic majors

--- src/ui/queries/ContactQueries.java
+++ src/ui/queries/ContactQueries.java
@@ -7,6 +7,9 @@ package ui.queries;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,13 +27,42 @@ public class ContactQueries {
         int count = 0;
         try{
         ResultSet result;
-        result = dataConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE).executeQuery("Select * From contacts");
+        result = dataConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE).executeQuery("SELECT * FROM contacts");
         result.last();
         count = result.getRow();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return count;
+    }
+    
+    public ResultSet getAcademicMajors(){        
+        ResultSet result = null;
+        try {
+            Statement statement = dataConnection.createStatement();
+            result = statement.executeQuery("SELECT * FROM majors ORDER BY major;");
+        } catch (SQLException ex) {
+            Logger.getLogger(ContactQueries.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    public void addAcademicMajor(String major){
+        try {
+            Statement statement = dataConnection.createStatement();
+            statement.execute("INSERT INTO majors(major) VALUES('" + major + "');");
+        } catch (SQLException ex) {
+            Logger.getLogger(ContactQueries.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+    
+    public void deleteAcademicMajor(String id){
+        try {
+            Statement statement = dataConnection.createStatement();
+            statement.execute("DELETE FROM majors WHERE id = " + id + ";");
+        } catch (SQLException ex) {
+            Logger.getLogger(ContactQueries.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
